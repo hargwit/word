129d55299a254e9ece7c24ac6262c9ffb89841c1
Harry_W <>
2019-10-04 08:42:47 +0100

    Added letter functionality.

--- src/components/letters/letter/letter.js
+++ src/components/letters/letter/letter.js
@@ -1,18 +1,47 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+
 import styled from '@emotion/styled'
 
 const Letter = ({ letter }) => {
+  const [showMenu, setShowMenu] = useState(false)
   const [selected, setSelected] = useState(false)
   const [rejected, setRejected] = useState(false)
 
   const SubComponent = selected ? SelectedLi : rejected ? RejectedLi : Li
 
+  function toggleMenu() {
+    setShowMenu(!showMenu)
+  }
   function toggleRejected() {
     setRejected(!rejected)
+    setSelected(false)
+    setShowMenu(false)
+  }
+  function toggleSelected() {
+    setSelected(!selected)
+    setRejected(false)
+    setShowMenu(false)
   }
 
-  return <SubComponent onClick={() => toggleRejected()}>{letter}</SubComponent>
+  const Menu = () =>
+    showMenu ? (
+      <div>
+        <button data-testid='select_button' onClick={() => toggleSelected()}>
+          ✓
+        </button>
+        <button data-testid='reject_button' onClick={() => toggleRejected()}>
+          ✗
+        </button>
+      </div>
+    ) : null
+
+  return (
+    <div>
+      <SubComponent onClick={() => toggleMenu()}>{letter}</SubComponent>
+      <Menu />
+    </div>
+  )
 }
 
 Letter.propTypes = {
--- src/components/letters/letter/letter.test.js
+++ src/components/letters/letter/letter.test.js
@@ -9,19 +9,59 @@ test('displays letter in props', () => {
   expect(getByText(/a/)).toBeInTheDocument()
 })
 
-test('toggles color to red and strike through on click', () => {
-  const { getByText } = render(<Letter letter='a' />)
+test('opens and closes menu on click', () => {
+  const { queryByTestId, getByText, getByTestId } = render(
+    <Letter letter='a' />,
+  )
 
-  expect(getByText(/a/)).toHaveStyleRule('color', 'black')
-  expect(getByText(/a/)).not.toHaveStyleRule('text-decoration', 'line-through')
+  expect(queryByTestId('select_button')).toBeNull()
+  expect(queryByTestId('reject_button')).toBeNull()
 
-  fireEvent.click(getByText(/a/))
+  fireEvent.click(getByText('a'))
 
-  expect(getByText(/a/)).toHaveStyleRule('color', 'red')
-  expect(getByText(/a/)).toHaveStyleRule('text-decoration', 'line-through')
+  expect(getByTestId('select_button')).toBeInTheDocument()
+  expect(getByTestId('reject_button')).toBeInTheDocument()
 
-  fireEvent.click(getByText(/a/))
+  fireEvent.click(getByText('a'))
 
-  expect(getByText(/a/)).toHaveStyleRule('color', 'black')
-  expect(getByText(/a/)).not.toHaveStyleRule('text-decoration', 'line-through')
+  expect(queryByTestId('select_button')).toBeNull()
+  expect(queryByTestId('reject_button')).toBeNull()
+})
+
+test('toggles color to red and strike through on reject', () => {
+  const { getByText, getByTestId } = render(<Letter letter='a' />)
+
+  expect(getByText('a')).toHaveStyleRule('color', 'black')
+  expect(getByText('a')).not.toHaveStyleRule('text-decoration', 'line-through')
+
+  fireEvent.click(getByText('a'))
+  fireEvent.click(getByTestId('reject_button'))
+
+  expect(getByText('a')).toHaveStyleRule('color', 'red')
+  expect(getByText('a')).toHaveStyleRule('text-decoration', 'line-through')
+
+  fireEvent.click(getByText('a'))
+  fireEvent.click(getByTestId('reject_button'))
+
+  expect(getByText('a')).toHaveStyleRule('color', 'black')
+  expect(getByText('a')).not.toHaveStyleRule('text-decoration', 'line-through')
+})
+
+test('toggles color to green and bold on select', () => {
+  const { getByText, getByTestId } = render(<Letter letter='a' />)
+
+  expect(getByText('a')).toHaveStyleRule('color', 'black')
+  expect(getByText('a')).not.toHaveStyleRule('font-weight', 'bold')
+
+  fireEvent.click(getByText('a'))
+  fireEvent.click(getByTestId('select_button'))
+
+  expect(getByText('a')).toHaveStyleRule('color', 'green')
+  expect(getByText('a')).toHaveStyleRule('font-weight', 'bold')
+
+  fireEvent.click(getByText('a'))
+  fireEvent.click(getByTestId('select_button'))
+
+  expect(getByText('a')).toHaveStyleRule('color', 'black')
+  expect(getByText('a')).not.toHaveStyleRule('font-weight', 'bold')
 })
